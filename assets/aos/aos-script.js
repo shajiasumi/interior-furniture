95b23fafd1aec0bac14ba6a778ffe3676d5ce9b8
Shajia sumi <shajiasumi33@gmail.com>
2026-02-15 15:15:22 +0600

    Add files via upload

---
+++ assets/aos/aos-script.js
@@ -0,0 +1,11 @@
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    });
+}
+window.addEventListener('load', () => {
+    aos_init();
+});
